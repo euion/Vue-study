0d78fbbcff4e25bf19c213f4710060a59bd6ee1b
0801euiyeon <qq212420@gmail.com>
2023-01-19 14:40:23 +0900

    study:inject,provide

---
+++ cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeBase.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <h2>Select a Topic</h2>
+    <knowledge-grid ></knowledge-grid>
+  </section>
+</template>
+
+<script>
+export default {
+  inject: ['topics'], /**상위 레벨에서 제공된 것만 삽입할 수 있다.  */
+  emits: ['select-topic'],
+};
+</script>
---
+++ cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeElement.vue
@@ -0,0 +1,15 @@
+<template>
+  <li>
+    <h3>{{ topicName }}</h3>
+    <p>{{ description }}</p>
+    <button @click="selectTopic(id)">Learn More</button>  <!--포인터로 가리키는 곳에 id를 제공해야 함-->
+  </li>
+</template>
+
+<script>
+export default {
+  inject:['selectTopic'],
+  props: ['id', 'topicName', 'description'],
+  emits: ['select-topic'],
+};
+</script>
